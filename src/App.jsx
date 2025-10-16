6c8d897f63cca0fd676037be7dbc92ca2857cfee
Alvin Lee <whyubotherme@gmail.com>
2025-10-16 16:07:16 +0800

    Test V3

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
     return (
         <div>
             <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server. V2.</p>
+            <p>This component demonstrates fetching data from the server. V3.</p>
             {contents}
         </div>
     );
